2c67faec1284d637403d5343193be39749d2f1d8
martinfournier <martinfou@gmail.com>
2016-03-24 00:02:02 -0400

    adding webappinitializer

---
+++ EventTracker/src/main/java/com/pluralsight/Customer.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class Customer {
+	String firstName;
+	int id;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
