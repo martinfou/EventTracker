e16b5a6f022279cb247d0481d8c81aad31f2aa93
martinfournier <martinfou@gmail.com>
2016-03-23 23:43:13 -0400

    web app without the need for a web.xml file

--- EventTracker/src/main/java/com/pluralsight/controller/HelloControlller.java
+++ EventTracker/src/main/java/com/pluralsight/controller/HelloControlller.java
@@ -9,6 +9,7 @@ public class HelloControlller {
 	
 	@RequestMapping(value= "/greeting")
 	public String sayHello(Model model){
+		
 		model.addAttribute("greeting", "Hello World");
 		
 		return "hello.jsp";
